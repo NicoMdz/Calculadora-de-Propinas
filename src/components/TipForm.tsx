757b48da1cd918fe12328cabb091c957fda0f97e
Nicolás <nick.mdz.b@gmail.com>
2024-03-31 15:50:54 -0600

    Primer Commit

---
+++ src/components/TipForm.tsx
@@ -0,0 +1,48 @@
+import type { Dispatch, SetStateAction } from "react";
+
+const tipOptions = [
+  {
+    id: "tip-10",
+    value: 0.1,
+    label: "10%",
+  },
+  {
+    id: "tip-20",
+    value: 0.2,
+    label: "20%",
+  },
+  {
+    id: "tip-50",
+    value: 0.5,
+    label: "50%",
+  },
+];
+
+type TipFormProps = {
+  setTip: Dispatch<SetStateAction<number>>;
+  tip: number
+};
+
+export default function TipForm({ setTip, tip }: TipFormProps) {
+  return (
+    <div>
+      <h3 className="font-black text-2xl">Propina:</h3>
+
+      <form>
+        {tipOptions.map((tipOption) => (
+          <div className="flex gap-2" key={tipOption.id}>
+            <label htmlFor={tipOption.id}>{tipOption.label}</label>
+            <input 
+                type="radio" 
+                name="tip" 
+                id={tipOption.id} 
+                value={tipOption.value}
+                onChange={ e => setTip(+e.target.value)} 
+                checked={tipOption.value === tip}
+            />
+          </div>
+        ))}
+      </form>
+    </div>
+  );
+}
